342a7f04f4dce1047158abe69786de2ee9533af1
Ben <benjt.mcmillan@icloud.com>
2024-01-11 22:05:34 -0500

    Worked on Drivetrain.java

---
+++ src/main/java/frc/robot/constants/CANBus.java
@@ -0,0 +1,8 @@
+package frc.robot.constants;
+
+public class CANBus {
+    public static final int RIGHT_MASTER = 1;
+    public static final int LEFT_MASTER = 2;
+    public static final int RIGHT_FOLLOWER = 3;
+    public static final int LEFT_FOLLOWER = 4;
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems;
+
+import frc.robot.constants.CANBus;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+
+public class Drivetrain extends SubsystemBase {
+    private final VictorSP rightMaster = new VictorSP(CANBus.RIGHT_MASTER);
+    private final VictorSP leftMaster = new VictorSP(CANBus.LEFT_MASTER);
+    private final VictorSP rightFollower = new VictorSP(CANBus.RIGHT_FOLLOWER);
+    private final VictorSP leftFollower = new VictorSP(CANBus.LEFT_FOLLOWER);
+
+    public Drivetrain(){
+        this.rightMaster.addFollower(this.rightFollower);
+        this.leftMaster.addFollower(this.leftFollower);
+
+        this.rightMaster.setInverted(true);
+        
+    }
+    
+    public void periodic() {
+        double translation = translationLimiter.calculate(targetTranslation);
+        double rotation = rotationLimiter.calculate(targetRotation);
+
+        double left = (translation + rotation);
+        double right = (translation - rotation);
+
+        setMotors(left, right);
+    }
+
+    private void setMotors(double left, double right) {
+        leftMaster.set(left);
+        rightMaster.set(right);
+    }
+
+    public vvoid arcadeDrive(double translation, double rotation) {
+        targetTranslation = translation;
+        targetRotation = rotation;
+    }
+} 
